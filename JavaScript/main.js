1937bdc62917ca04a0abd04e74844b207152c4f3
NerviMateo <antoniojmateoc@gmail.com>
2017-02-07 18:05:09 +0100

    Exercise 9.1

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,6 +1,25 @@
 (function(){
   'use strict';
 }());
-var header = document.getElementById('tablaimagen');
-  header.style = 'color: red';
-  console.log('header = ', header.getAttribute('style'));
+  var $myBtn = $('h2');
+
+  // Le añado un listener (o disparador), cuando se haga click
+  $myBtn.click(function(){
+    var classList = '';
+
+    if($myBtn.hasClass('active')){
+      // Si myBtn contiene la clase active la elimino
+      $(this).removeClass('active');
+
+      // Recuperamos el atributo clase y los partimos
+      classList = $('#header').attr('class').split(' ');
+      console.log('classList === ', classList);
+    }else{
+      // En caso contrario la añado
+      $(this).addClass('active');
+
+      // Recuperamos el atributo clase y los partimos
+      classList = $('#header').attr('class').split(' ');
+      console.log('classList === ', classList);
+    }
+  });
