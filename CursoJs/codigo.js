bb5091f2b64d4a459d250334b6967c2f69529244
NerviMateo <antoniojmateoc@gmail.com>
2017-02-13 11:57:42 +0100

    Ejercicio 2 JavaScript

---
+++ CursoJs/codigo.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict';
+}());
+//Aqui funciona el primer script en el que saldra el Hola mundo
+type='text/javascript'
+alert("Hola Mundo!");
+/* Y aqui saldra en cambio
+el siguiente que hara que salga
+en pantalla algo nuevo
+un mensaje que pone alert */
+type='text/javascript'
+alert("Soy el primer script");
